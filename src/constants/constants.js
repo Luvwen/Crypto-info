e426a80be03b77f0b12258bde539add40f82b85d
Santiago <santiagobalino@hotmail.com>
2023-01-28 22:21:21 -0300

    Update gitignore and add constants file

---
+++ src/constants/constants.js
@@ -0,0 +1,26 @@
+export const CRYPTO_FAQ_INFO = [
+    {
+        question: 'What is Cryptocurrency?',
+        answer: 'Discover what crypto is and how it works. A cryptocurrency is a digital form of money. They are digital assets created to be used to pay for goods and services. Cryptocurrencies can be used as a store of value, unit of account and medium of exchange. These digital assets are secured on a distributed ledger called the “blockchain.” The first cryptocurrency was Bitcoin which was introduced in a whitepaper by pseudonymous creator Satoshi Nakamoto. It was launched in 2009. The digital asset has since served as the benchmark crypto asset.',
+    },
+    {
+        question: 'How to Buy Cryptocurrency',
+        answer: 'You can buy cryptocurrency in several ways. The most popular is to buy cryptocurrency on a regulated cryptocurrency exchange like Coinbase. Another method is to buy cryptocurrencies like Bitcoin via a cryptocurrency automated teller machine (ATM). Using an exchange is often the most convenient, but you might have to go through an extensive know-your-customer (KYC) process before accessing the platform.',
+    },
+    {
+        question: 'How does Cryptocurrency Work?',
+        answer: 'Cryptocurrency works with the help of the blockchain, miners or validators and the users. Once one user sends a cryptocurrency to another, cryptocurrency miners validate and then add the transaction to the blockchain. Each transaction is cryptographically encoded and validator nodes compete to solve this cryptographic puzzle. Once this happens, the receiver will see the funds in their wallet balance.',
+    },
+    {
+        question: 'What is Crypto Mining?',
+        answer: 'Crypto mining is the process of securing and validating transactions on a blockchain network. The Proof-of-Work (PoW) is the first recognised mining protocol and requires validators to compete in solving complex mathematical equations. The miner who gets the correct response would be eligible to add the transactions to the network and as such, receive the block rewards - in the form of newly minted Bitcoins. PoW mining requires specialised and expensive mining hardware to pull off, and this equipment consumes more energy and is difficult to maintain.',
+    },
+    {
+        question: 'What is Staking Crypto?',
+        answer: 'Staking crypto entails locking up your crypto asset to secure a particular network. Crypto staking is common with cryptocurrencies that use a Proof-of-Stake (PoS) consensus algorithm. In comparison, assets like Bitcoin use the Proof-of-Work (PoW) model to validate and secure transactions on their network, while others like Cardano and Polkadot use the PoS model. Crypto staking is a great way to earn passive income, and it is also a more energy-efficient model to mine new coins for the network. Due to its consistent yields and low barrier to entry, many crypto exchanges now offer Staking-as-a-Service (SaaS) to their users.',
+    },
+    {
+        question: 'How does Cryptocurrency gain value?',
+        answer: 'Cryptocurrency gains value through adoption and market speculators. If the demand for a digital asset is high, then the value would rise. The more people who are willing to use a digital asset, the more likely it will increase in value and prominence. The most valuable cryptocurrencies are those with more adoption and high demand. These cryptocurrencies are digital assets for the long term.',
+    },
+];
